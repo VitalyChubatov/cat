bf4860e3700a8ab91e701059a17e6aff2b9036dc
houldC <chubatovvv@bk.ru>
2024-01-30 12:26:37 +0300

    add styles header

--- src/Components/LoveCat/LoveCat.jsx
+++ src/Components/LoveCat/LoveCat.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import styles from '../Main/main.module.css';
 import { Cat } from '../Cat';
+import { Link } from 'react-router-dom';
 
 export const LoveCat = ({ catHeart, deleteCat }) => {
 	return (
-		<div className={styles.wrapper}>
-			{catHeart.map(cat => (
-				<Cat cat={cat} catHeart={catHeart} deleteCat={deleteCat} key={cat.id} />
-			))}
-		</div>
+		<>
+			{catHeart.length === 0 ? (
+				<h3>
+					Добавьте котиков во вкладке
+					<hr />
+					<Link to='/' className={styles.catHref}>
+						Все котики
+					</Link>
+				</h3>
+			) : (
+				<div className={styles.wrapper}>
+					{catHeart.map(cat => (
+						<Cat
+							cat={cat}
+							catHeart={catHeart}
+							deleteCat={deleteCat}
+							key={cat.id}
+						/>
+					))}
+				</div>
+			)}
+		</>
 	);
 };
